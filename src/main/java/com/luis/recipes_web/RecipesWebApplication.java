5dd78bec824edd491bfd22c917b8e8ec99a7a0c9
luisZamudio1501 <luiszamudio150115@gmail.com>
2026-01-01 02:45:35 -0300

    init spring boot project

---
+++ src/main/java/com/luis/recipes_web/RecipesWebApplication.java
@@ -0,0 +1,15 @@
+package com.luis.recipes_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RecipesWebApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/luis/recipes_web/RecipesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.recipes_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
